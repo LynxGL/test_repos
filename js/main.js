212a8a77c2c4a263e6c3f5f79464e7f4887c2991
Danya <danilamelin58@gmail.com>
2024-01-31 21:06:54 +0200

    some changes in main js. was added theme synchron. language on the date was changed.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,57 @@ const modeBtn = document.querySelector('.mode-switch');
 const lightSun = document.querySelector('.sun.light');
 const darkMoon = document.querySelector('.moon.dark');
 
+const body = document.body;
 
-modeBtn.onclick = function () {
+let mode = 'light';
 
-    let isLightMoonHidden = lightSun.classList.contains('hidden');
+if (!localStorage.getItem('mode')) {
+    localStorage.setItem('mode', mode);
+} else {
+    mode = localStorage.getItem('mode');
+}
 
-    if (isLightMoonHidden) {
+if (mode === 'dark') {
+    changeMode(mode);
+}
+
+if (modeBtn && lightSun && darkMoon) {
+
+    modeBtn.addEventListener('click', function () {
+
+        let isLightMoonHidden = lightSun.classList.contains('hidden');
+
+        if (isLightMoonHidden) {
+            lightSun.classList.remove('hidden');
+            darkMoon.classList.add('hidden');
+            changeMode('dark');
+        } else {
+            lightSun.classList.add('hidden');
+            darkMoon.classList.remove('hidden');
+            changeMode('light');
+        }
+        localStorage.setItem('mode', mode);
+    });
+
+} else {
+    console.error("One or more elements of dark-mode not found!");
+}
+
+function changeMode(newMode) {
+    if (newMode === 'dark') {
+        body.setAttribute('dark-mode', '');
         lightSun.classList.remove('hidden');
         darkMoon.classList.add('hidden');
-        document.body.setAttribute('dark', '');
-    } else {
-        lightSun.classList.add('hidden');
-        darkMoon.classList.remove('hidden');
-        document.body.removeAttribute('dark');
+        mode = 'dark';
+    }
+    else {
+        body.removeAttribute('dark-mode');
+        mode = 'light';
     }
 }
+
+
+
 
 
 
